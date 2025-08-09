5d145d4ae254fcf9685cc707c658358a266a12e4
Santiago Andres Gonzalez Rodríguez <sgonzalez@wearenovae.com>
2025-08-09 17:43:03 -0500

    feat(reservas): implementar vista de mis reservas con opción de cancelación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ListadoClasesComponent } from './features/clases/listado-clases/listado-clases.component';
 import { DetalleClaseComponent } from './features/clases/detalle-clase/detalle-clase.component';
+import { MisReservasComponent } from './features/reservas/mis-reservas/mis-reservas/mis-reservas.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'clases', pathMatch: 'full' },
     { path: 'clases', component: ListadoClasesComponent },
     { path: 'clases/:id', component: DetalleClaseComponent },
+    { path: 'reservas', component: MisReservasComponent },
     { path: '**', redirectTo: 'clases' }
 ];
